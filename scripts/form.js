b84be418af12a84ea8f682626e54f138921fe5e0
Svekla89 <ivanovskaya89@gmail.com>
2021-02-10 21:14:56 +0300

    задисейблены формы lastname и firstname

--- scripts/form.js
+++ scripts/form.js
@@ -1,14 +1,21 @@
 const onChangeUserData = (event, fieldName) => {
-  console.log(event)
-  const loginField = document.querySelector('input[name=login]')
+  
+  const firstnameField = document.querySelector('input[name][0]')
+  const lastnameField = document.querySelector('input[name][1]')
   // loginField.value = loginField.value + event.data // TODO: BUGS!!
   //loginField.value = event.target.value
 
-  if(fieldName === 'firstname'){
-    loginField.value = event.target.value + '_' + document.querySelector('input[name=lastname]').value
+  firstnameField.value = event.target.value
+  console.log(firstnameField)
+
+  /*if(fieldName === 'firstname'){
+
+
+    loginField.value /* то что находится в поле логин = event.target.value + '_' + document.querySelector('input[name=lastname]').value
   } else {
     loginField.value = document.querySelector('input[name=firstname]').value + '_' + event.target.value
   }
+  */
 }
 
 const firstName = document.querySelector('input[name=firstname]')
@@ -16,5 +23,7 @@ const lastName = document.querySelector('input[name=lastname]')
 
 firstName.addEventListener('input', e => onChangeUserData(e, 'firstname'))
 lastName.addEventListener('input', e => onChangeUserData(e, 'lastname'))
+
+//если вводишь логин то появляется в имени и фамилии
 
 
