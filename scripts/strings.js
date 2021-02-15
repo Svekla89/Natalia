127e4405fdd2cba8e6129c3851d92fba602afdb1
Svekla89 <ivanovskaya89@gmail.com>
2021-02-15 16:18:03 +0300

    провелили почту на валидность

--- scripts/strings.js
+++ scripts/strings.js
@@ -5,5 +5,15 @@ if(phoneText && phoneText[0]=== "8"){
     console.log(event.target.value)
 }   
 } 
-const tel = document.querySelector("input[type=tel]")
-tel.addEventListener("blur", event => changePhoneNumber(event))//навесили событие потери фокуса на телефон
+const email = document.querySelector("input[type=email]")
+email.addEventListener("blur", event => changePhoneNumber(event))
+
+const changeEmail = (event)=>{
+    const emailText = event.target.value
+    if(emailText === "/.+@.+\..+"){
+        event.target.value = true
+        console.log(event.target.value)
+    }   
+    } 
+    const email = document.querySelector("input[type=email]")
+    email.addEventListener("blur", event => changeEmail(event))//проверить валидность почты
