01bace7ec7fedf591fa92f1b1fee3ec74e15e696
greyewi1 <greyewi@gmail.com>
2021-02-16 18:38:39 +0200

    add validation email

--- scripts/strings.js
+++ scripts/strings.js
@@ -1,19 +1,23 @@
-const changePhoneNumber = (event)=>{
-const phoneText = event.target.value
-if(phoneText && phoneText[0]=== "8"){
-    event.target.value = phoneText.replace("8","+7")
+const changePhoneNumber = (event) => {
+  const phoneText = event.target.value
+  if (phoneText && phoneText[0] === "8") {
+    event.target.value = phoneText.replace("8", "+7")
     console.log(event.target.value)
-}   
-} 
-const email = document.querySelector("input[type=email]")
-email.addEventListener("blur", event => changePhoneNumber(event))
+  }
+}
+//const email = document.querySelector("input[type=email]")
+//email.addEventListener("blur", event => changePhoneNumber(event))
 
-const changeEmail = (event)=>{
-    const emailText = event.target.value
-    if(emailText === "\.+@.+\..+"){
-        event.target.value = true
-        console.log(event.target.value)
-    }   
-    } 
-    const email = document.querySelector("input[type=email]")
-    email.addEventListener("blur", event => changeEmail(event))//проверить валидность почты
+const changeEmail = (event) => {
+  const emailText = event.target.value
+  // if(!emailText.match(/.+@.+\..+/)){ // проверка на валидность регулярного выражения
+  //   alert("Невалидная почта")
+  // }
+
+  if(!/.+@.+\..+/.test(emailText)){ // проверка на валидность строки
+    alert("Невалидная почта")
+  }
+}
+
+const email = document.querySelector("input[type=email]")
+email.addEventListener("blur", event => changeEmail(event))//проверить валидность почты
